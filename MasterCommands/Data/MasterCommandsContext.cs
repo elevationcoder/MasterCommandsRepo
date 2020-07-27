cfa99b5268c100d90e5c7b49613ebf43448ba1dc
Ian Marrs <ian.t.marrs@gmail.com>
2020-07-27 13:24:11 -0600

    Added DbContext to program using entity framework

---
+++ MasterCommands/Data/MasterCommandsContext.cs
@@ -0,0 +1,19 @@
+﻿using MasterCommands.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterCommands.Data
+{
+    public class MasterCommandsContext : DbContext
+    {
+        public MasterCommandsContext(DbContextOptions<MasterCommandsContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Command> Commands { get; set; }
+    }
+}
