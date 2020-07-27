904117c4b92822353174fefb84db2f1fbeeea455
Ian Marrs <ian.t.marrs@gmail.com>
2020-07-27 10:23:08 -0600

    Created and added Program interface and Mock interface, added Command model and Commands Controller

---
+++ MasterCommands/Data/IMasterCommandsRepo.cs
@@ -0,0 +1,14 @@
+﻿using MasterCommands.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterCommands.Data
+{
+    public interface IMasterCommandsRepo
+    {
+        IEnumerable<Command> GetAllCommands();
+        Command GetCommandById(int id);
+    }
+}
---
+++ MasterCommands/Data/MockMasterCommandsRepo.cs
@@ -0,0 +1,15 @@
+﻿using MasterCommands.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterCommands.Data
+{
+    public class MockMasterCommandsRepo : IMasterCommandsRepo
+    {
+        public IEnumerable<Command> GetAllCommands();
+
+        public
+    }
+}
