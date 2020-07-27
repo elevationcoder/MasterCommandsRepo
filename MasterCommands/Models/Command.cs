f1b7a47dc0b6785c1f1e79cff4db3c174b1d925e
Ian Marrs <ian.t.marrs@gmail.com>
2020-07-27 14:07:08 -0600

    added required validations to Command Model

--- MasterCommands/Models/Command.cs
+++ MasterCommands/Models/Command.cs
@@ -1,10 +1,15 @@
-﻿namespace MasterCommands.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MasterCommands.Models
 {
     public class Command
     {
         public int Id { get; set; }
+        [Required]
         public string HowTo { get; set; }
+        [Required]
         public string Line { get; set; }
+        [Required]
         public string Platform { get; set; }
     }
 }
