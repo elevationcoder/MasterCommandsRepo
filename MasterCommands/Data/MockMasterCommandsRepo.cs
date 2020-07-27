0c08a1181779703b889d480a89eae73d0e6332a2
Ian Marrs <ian.t.marrs@gmail.com>
2020-07-27 10:24:34 -0600

    made minor changes to csproj file and added Commands Controller

--- MasterCommands/Data/MockMasterCommandsRepo.cs
+++ MasterCommands/Data/MockMasterCommandsRepo.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace MasterCommands.Data
 {
-    public class MockMasterCommandsRepo : IMasterCommandsRepo
-    {
-        public IEnumerable<Command> GetAllCommands();
+    public class MockMasterCommandsRepo : IMasterCommandsRepo { 
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+            {
+               new Command{Id=0, HowTo="Boil an egg", Line="Boil water", Platform="Kettle and Pan"},
+               new Command{Id=1, HowTo="Mow lawn", Line="Start mower", Platform="Lawn Mower"},
+               new Command{Id=2, HowTo="Run", Line="Take long strides at a fast pace", Platform="Sidewalk"}
+            };
 
-        public
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle and Pan" };
+        }
     }
 }
