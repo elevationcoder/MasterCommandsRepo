54a8cc5f84c73e99cbd5c05f08d8ddd3fbaf7c43
Ian Marrs <ian.t.marrs@gmail.com>
2020-07-27 13:41:04 -0600

    added DbContext connection string to Startup configure services

--- MasterCommands/Startup.cs
+++ MasterCommands/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +29,8 @@ namespace MasterCommands
         {
             services.AddControllers();
             services.AddScoped<IMasterCommandsRepo, MockMasterCommandsRepo>();
+            services.AddDbContext<MasterCommandsContext>(opt => opt.UseSqlServer
+            (Configuration.GetConnectionString("MasterCommandsConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
