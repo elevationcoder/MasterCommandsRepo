3c45490e7a228ab9064b30e0ab9b1890436d1456
Ian Marrs <ian.t.marrs@gmail.com>
2020-07-27 18:11:42 -0600

    Created Commands Profile

---
+++ MasterCommands/Profiles/CommandsProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MasterCommands.DTOs;
+using MasterCommands.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterCommands.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            CreateMap<Command, MasterCommandsReadDto>();
+        }
+    }
+}
