6ad277368786f00554bc859e2d45e4ed8f928c89
Ian Marrs <ian.t.marrs@gmail.com>
2020-07-27 18:06:36 -0600

    Created MasterCommands Read DTO

---
+++ MasterCommands/DTOs/MasterCommandsReadDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterCommands.DTOs
+{
+    public class MasterCommandsReadDto
+    {
+        public int Id { get; set; }
+        public string HowTo { get; set; }
+        public string Line { get; set; }
+    }
+}
